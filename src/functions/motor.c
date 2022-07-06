37065c238bf29c2cc41264707b11ef7881113aa4
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-06 20:53:47 -0300

    FIX. moved motor comment states to motor.c

--- src/functions/motor.c
+++ src/functions/motor.c
@@ -1,5 +1,19 @@
 #include "helper.h"
 
+//motor1
+// set_bit(PORTC, IN1);
+// clr_bit(PORTC, IN2);
+
+// clr_bit(PORTC, IN1);
+// set_bit(PORTC, IN2);
+
+// //motor2
+// set_bit(PORTC, IN3);
+// clr_bit(PORTC, IN4);
+
+// clr_bit(PORTC, IN3);
+// set_bit(PORTC, IN4);
+
 void turnRight() {
   set_bit(PORTC, IN1);
 	clr_bit(PORTC, IN2);
